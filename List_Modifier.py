3c0e995313478a06edd6aaa6a62157cde4f0f392
unknown <Christiandevega12@gmail.com>
2022-11-05 11:11:12 +0800

    add 2nd and 3rd option

--- List_Modifier.py
+++ List_Modifier.py
@@ -1,5 +1,5 @@
 def Main():
-    Array=[1, 4, 3, 4, 4, 5, 6 ,2 ,8,26,4,50,8,32,97,8,3,1,1,13,22,2,6,2,60,65,2,76,62,0,57,23,53,8]
+    Array=[1, 4, 3, 4, 4, 5, 6, 2, 8, 26, 4, 50, 8, 32, 97 ,8 ,3 ,1 ,1 ,13 ,22 ,2 ,6 ,2 ,60 ,65 ,2 ,76 ,62 ,0 ,57 ,23 ,53 ,8]
     
     def Menu_Intro(Array):
         
@@ -8,7 +8,7 @@ def Main():
         print("Menu:")
         print("1 -> Add an element")
         print("2 -> Insert an element")
-        print("3 -> Modify an element")
+        print("3 -> Count the number of an element")
         print("4 -> Delete an element")
         print("5 -> Arrange in ascending order")
         print("6 -> Arrange in descending order")
@@ -22,15 +22,25 @@ def Main():
                 Array.append(user_add)
                 print(Array)
         if Users_num_input == 2:
-                print("You chose 2")
+                print("You chose the Insert an element option")
+                user_element=int(input("enter the element to be added: "))
+                user_Index=int(input("enter the index for the element: "))
+                Array.insert(user_Index,user_element)
+                print(Array)
         if Users_num_input == 3:
-                print("You chose 3")
+                print("You chose the Count the number of an element option")
+                user_count=int(input("Pick an element to be counted: "))
+                Array_counter=Array.count(user_count)
+                print(Array_counter)                
         if Users_num_input == 4:
-                print("You chose 4")
+                print("You chose the Delete an element option")
+                user_add=int(input("Add an element to the list: "))
         if Users_num_input == 5:
-                print("You chose 5")
+                print("You chose the Arrange in ascending order option")
+                user_add=int(input("Add an element to the list: "))
         if Users_num_input == 6:
-                print("You chose 6")
+                print("You chose Arrange in descending order option")
+                user_add=int(input("Add an element to the list: "))
         return 
     return Menu_Intro(Array), User_Input(Array)
 
